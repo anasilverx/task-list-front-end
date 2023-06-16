fe89b8480582be9e39621706d2a3488c9b99db4c
Ana Silvera <anasilverx@gmail.com>
2023-06-16 14:28:04 -0700

    refactored task component to use props

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  // const [complete, setComplete] = useState(isComplete);
-  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={isComplete}
-        // onClick={() => setComplete(!complete)}
+        onClick={props.isComplete}
       >
-        {title}
+        {props.title}
       </button>
       <button className="tasks__item__remove button">x</button>
     </li>
