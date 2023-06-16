3b74a08fa709320d4e7eca9c01a3fbb0b965d344
Ana Silvera <anasilverx@gmail.com>
2023-06-16 14:25:03 -0700

    removed state from Task. added state to App for task isComplete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
-const TASKS = [
+const INITIAL_TASKS = [
   {
     id: 1,
     title: 'Mow the lawn',
@@ -16,13 +16,27 @@ const TASKS = [
 ];
 
 const App = () => {
+
+  const [tasks, setTasks] = useState(INITIAL_TASKS);
+
+  const toggleComplete = (taskId) => {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        task.isComplete = !task.isComplete;
+      }
+      return { ...task };
+    });
+    
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={INITIAL_TASKS} toggleComplete={toggleComplete} />}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+  // const [complete, setComplete] = useState(isComplete);
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={isComplete}
+        // onClick={() => setComplete(!complete)}
       >
         {title}
       </button>
