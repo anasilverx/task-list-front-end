1bcecf2bdb58952b81c532fbfa980428d182f23b
Lyuda Kim <lyudarkim@gmail.com>
2023-06-21 15:13:55 -0700

    Refactor updateComplete to make PATCH request to back-end.

--- src/App.js
+++ src/App.js
@@ -8,46 +8,55 @@ const TASK_URL = 'https://task-list-api-302r.onrender.com/tasks';
 const App = () => {
 // tasks defined in useState is passed as prop to TaskList
   const [tasks, setTasks] = useState([]);
-
+  
+  const getTasks = () => {
+    axios
+    .get(TASK_URL)
+    .then((response) => {
+      const newTasks = response.data.map((task) => {
+        return {
+          'description': task.description,
+          'id': task.id,
+          'title': task.title,
+          'isComplete': task.is_complete,
+        };
+      });
+      setTasks(newTasks);
+    })
+    .catch((error) => {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    });
+  };
+  
+  useEffect(() => {
+    getTasks();
+  }, []);
+  
   const updateComplete = (taskId) => {
     const updatedTasks = tasks.map(task => {
       if (task.id === taskId) {
         task.isComplete = !task.isComplete;
+        let routeWord = 'mark_complete';
+        if (!task.isComplete) {
+          routeWord = 'mark_incomplete';
+        }
+        axios
+          .patch(`${TASK_URL}/${taskId}/${routeWord}`)
+          .then(() => console.log(`Marked ${routeWord}`))
+          .catch((error) => console.log(error));
       }
-      return { ...task };
+
+      return task;
     });
     
     setTasks(updatedTasks);
   };
-
-  const getTasks = () => {
-    axios
-      .get(TASK_URL)
-      .then((response) => {
-        const newTasks = response.data.map((task) => {
-          return {
-            'description': task.description,
-            'id': task.id,
-            'title': task.title,
-            'isComplete': task.is_complete,
-          };
-        });
-        setTasks(newTasks);
-      })
-      .catch((error) => {
-        console.log(error.response.status);
-        console.log(error.response.data);
-      });
-  };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
+  
   const updateDelete = (taskId) => {
     axios
-      .delete(`${TASK_URL}/${taskId}`)
-      .then((response) => {
+    .delete(`${TASK_URL}/${taskId}`)
+    .then((response) => {
         console.log(response);
 
         const newTasks = [];
@@ -64,6 +73,8 @@ const App = () => {
       });
   };
   
+
+
 
   return (
     <div className="App">
